91ca30acc187ccb41f18bd1c9f5d397407afe53b
sleepysleep <60518847+sleepysleep@users.noreply.github.com>
2020-02-06 23:43:45 +0530

    Adding Scanner object

--- Hello_World.java
+++ Hello_World.java
@@ -4,6 +4,10 @@ class Hello_World
 {
   public static void main(String args[])
   {
+    Scanner sc=new Scanner(System.in);
     System.out.println("Hello_World");
+    System.out.println("Enter an integer value : ");
+    int x=sc.nextInteger();
+    System.out.println("The entered value is  : a = "+a);
   }
 }
